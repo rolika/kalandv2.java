3c32268f19cb0f102e850ca198b80cd006119b8d
rolika <weisz.roland@invitel.hu>
2017-03-21 20:05:49 +0100

    játék vége kiszervezve

--- src/kaland/Kaland.java
+++ src/kaland/Kaland.java
@@ -6,42 +6,40 @@ import org.apache.commons.lang3.text.WordUtils;
 
 /**
  * Szöveges kalandjáték Java konzolon
- * 
+ *
  * @author rolika
  */
 public class Kaland {
-  
+
   static final int WRAP = 80; // sortörés szélessége
   static Jatekos jatekos;
   static StringBuilder jatekSzoveg;
+  static Scanner bevitel = new Scanner(System.in);
 
   public static void main(String[] args) {
-    
-    Scanner bevitel = new Scanner(System.in);
-    
-    
+
     while (true) {
-      
+
       ujJatek();
-      
+
       while (!(jatekos.isMeghalt() && jatekos.isNyert() && jatekos.isVesztett())) { // játékciklus
-        
+
         /*
         Játékos helyzete
-        */
+         */
         jatekSzoveg = new StringBuilder();
         // helyszín leírás befűzése
         sortoro(jatekos.getHelyszin().getLeiras());
         jatekos.getHelyszin().setBejart(true);
         // látható tárgyak befűzése
-        // ellenségek befűzése 
+        // ellenségek befűzése
         System.out.print(jatekSzoveg.toString());
-      
+
         /*
         Játékos utasításának beolvasása és kezelése
-        */
+         */
         jatekSzoveg = new StringBuilder();
-        Set<SzotarInterface> parancsszavak = Ertelmezo.szetbont(utasitas(bevitel));
+        Set<SzotarInterface> parancsszavak = Ertelmezo.szetbont(utasitas());
         if (parancsszavak.remove(ParancsEnum.KILEP)) {
           break;
         } else if (parancsszavak.remove(IranyEnum.ESZAK)) {
@@ -61,50 +59,63 @@ public class Kaland {
         } else {
           sortoro(UzenetEnum.NEM_ERTEM.toString());
         }
-        
+
         System.out.print(jatekSzoveg.toString());
-      }
-      
-      /*
-      A játék - így vagy úgy - végetért
-      */
-      jatekSzoveg = new StringBuilder();
-      if (jatekos.isMeghalt()) {
-        sortoro(UzenetEnum.MEGHALT.toString());
-      } else if (jatekos.isNyert()) {
-        sortoro(UzenetEnum.NYERT.toString());
-      } else {
-        sortoro(UzenetEnum.VESZTETT.toString());
-      }
-      sortoro(UzenetEnum.UJ_JATEK.toString());
-      System.out.print(jatekSzoveg.toString());
-      Set<SzotarInterface> valasz = Ertelmezo.szetbont(utasitas(bevitel));
-      if (!valasz.remove(ParancsEnum.MEGEROSIT)) {
+        
+      } // játékciklus vége
+
+      if (jatekVege()) {
         break;
-      }      
+      }
     }
-    
+
   }
-  
-  static String utasitas(Scanner bevitel) {
+
+  /**
+   * Kiírja a promptot és beolvassa a játékos utasításait
+   *
+   * @return játékos által begépelt utasítás
+   */
+  static String utasitas() {
     System.out.print("- ");
     return bevitel.nextLine();
   }
-  
+
   /**
    * Új játékot indít, mindent alaphelyzetbe állítva.
    */
   static void ujJatek() {
     jatekos = new Jatekos(HelyszinEnum.HAZ_ELOTT);
   }
-  
+
   /**
-   * Az adott szöveget meghatározott szélességű sorokra tördeli a szóhatárokon,
-   * és beilleszt egy újsort is az egész mögé.
-   * @param szoveg 
+   * A játék véget ért, kiírja a megfelelő üzenetet, ill. felkér egy újabb játékra
+   *
+   * @return igaz, ha még játszani akar a játékos
+   */
+  static boolean jatekVege() {
+    jatekSzoveg = new StringBuilder();
+    if (jatekos.isMeghalt()) {
+      sortoro(UzenetEnum.MEGHALT.toString());
+    } else if (jatekos.isNyert()) {
+      sortoro(UzenetEnum.NYERT.toString());
+    } else {
+      sortoro(UzenetEnum.VESZTETT.toString());
+    }
+    sortoro(UzenetEnum.UJ_JATEK.toString());
+    System.out.print(jatekSzoveg.toString());
+    Set<SzotarInterface> valasz = Ertelmezo.szetbont(utasitas());
+    return !valasz.remove(ParancsEnum.MEGEROSIT);
+  }
+
+  /**
+   * Az adott szöveget meghatározott szélességű sorokra tördeli a szóhatárokon, és beilleszt egy
+   * újsort is az egész mögé.
+   *
+   * @param szoveg
    */
   static void sortoro(String szoveg) {
     jatekSzoveg.append(WordUtils.wrap(szoveg + "\n", WRAP));
   }
-  
+
 }
