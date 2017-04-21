987d63a91e848821439b60dcc56ec30355b4c97a
rolika <weisz.roland@invitel.hu>
2017-04-21 19:43:40 +0200

    ajtószótár hozzáadva

--- src/kaland/AjtoSzotarEnum.java
+++ src/kaland/AjtoSzotarEnum.java
@@ -1,16 +1,31 @@
 package kaland;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author rolika
  */
 enum AjtoSzotarEnum implements SzotarInterface {
 
-  ;
+  BEJARATI_AJTO("ajtó", "ajtót", "ajtóval"),
+  SZOBAAJTO("ajtó", "ajtót", "ajtóval", "szobaajtó", "szobaajtót", "szobaajtóval", "faajtó", "faajtót", "faajtóval"),
+  KONYHAAJTO("ajtó", "ajtót", "ajtóval", "konyhaajtó", "konyhaajtót", "konyhaajtóval", "üvegajtó", "üvegajtót", "üvegajtóval"),
+  VAKAJTO("boltív", "boltívet", "boltívvel", "fal", "falat", "fallal", "vakajtó", "vakajtót", "vakajtóval"),
+  LADA("láda", "ládát", "ládával", "faláda", "faládát", "faládával"),
+  PORTAL("portál", "portált", "portállal", "kőlap", "kőlapot", "kőlappal");
 
+  private final Set<String> szinonimak;
+
+  private AjtoSzotarEnum(String... szinonimak) {
+    this.szinonimak = new HashSet<>(Arrays.asList(szinonimak));
+  }
+  
   @Override
   public SzotarInterface getSzoEnum(String szo) {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return szinonimak.contains(szo) ? this : null;
   }
   
 }
