5450b0a196a5856033f959e385c1210d30c057db
rolika <weisz.roland@invitel.hu>
2017-04-22 10:10:41 +0200

    ajtók több állapota

--- src/kaland/AjtoEnum.java
+++ src/kaland/AjtoEnum.java
@@ -1,5 +1,7 @@
 package kaland;
 
+import com.google.common.collect.Sets;
+import java.util.Arrays;
 import java.util.EnumSet;
 
 /**
@@ -11,31 +13,37 @@ import java.util.EnumSet;
 
 enum AjtoEnum implements ElemInterface {
 
-  BEJARATI_AJTO("bejárati ajtó", "Ez egy bejárati ajtó.", AllapotEnum.CSUKVA, TargyEnum.KULCS,
-    HelyszinEnum.HAZ_ELOTT, HelyszinEnum.ELOTER),
-  SZOBAAJTO("szobaajtó", "Ez egy szobaajtó.", AllapotEnum.CSUKVA, null,
-    HelyszinEnum.FOLYOSO, HelyszinEnum.SZOBA),
-  KONYHAAJTO("konyhaajtó", "Ez egy konyhaajtó.", AllapotEnum.CSUKVA, null,
-    HelyszinEnum.FOLYOSO, HelyszinEnum.KONYHA),
-  VAKAJTO("vakajtó", "Ez egy vakajtó", AllapotEnum.ZARVA, null,
-    HelyszinEnum.PINCE, HelyszinEnum.REJTETT_PINCE),
-  LADA("láda", "Ez egy láda.", AllapotEnum.ZARVA, TargyEnum.BICSKA,
-    HelyszinEnum.PADLAS_VEGE, HelyszinEnum.REJTETT_PINCE),
-  PORTAL("portál", "Ez egy portál.", AllapotEnum.ZARVA, TargyEnum.BICSKA,
-    HelyszinEnum.REJTETT_PINCE, HelyszinEnum.ODAAT);
+  BEJARATI_AJTO("bejárati ajtó", "Ez egy bejárati ajtó.", TargyEnum.KULCS,
+    HelyszinEnum.HAZ_ELOTT, HelyszinEnum.ELOTER,
+    AllapotEnum.CSUKVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO),
+  SZOBAAJTO("szobaajtó", "Ez egy szobaajtó.", null,
+    HelyszinEnum.FOLYOSO, HelyszinEnum.SZOBA,
+    AllapotEnum.CSUKVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO),
+  KONYHAAJTO("konyhaajtó", "Ez egy konyhaajtó.", null,
+    HelyszinEnum.FOLYOSO, HelyszinEnum.KONYHA,
+    AllapotEnum.CSUKVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO),
+  VAKAJTO("vakajtó", "Ez egy vakajtó", null,
+    HelyszinEnum.PINCE, HelyszinEnum.REJTETT_PINCE,
+    AllapotEnum.ZARVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO),
+  LADA("láda", "Ez egy láda.", TargyEnum.BICSKA,
+    HelyszinEnum.PADLAS_VEGE, HelyszinEnum.REJTETT_PINCE,
+    AllapotEnum.ZARVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO),
+  PORTAL("portál", "Ez egy portál.", TargyEnum.BICSKA,
+    HelyszinEnum.REJTETT_PINCE, HelyszinEnum.ODAAT,
+    AllapotEnum.ZARVA, AllapotEnum.LATHATO, AllapotEnum.NYITHATO);
 
   private final String nev, leiras;
   private final EnumSet<AllapotEnum> allapot;
   private final TargyEnum kulcs;
   private final EnumSet<HelyszinEnum> hely;
 
-  private AjtoEnum(String nev, String leiras, AllapotEnum allapot, TargyEnum kulcs,
-    HelyszinEnum egyik, HelyszinEnum masik) {
+  private AjtoEnum(String nev, String leiras, TargyEnum kulcs,
+    HelyszinEnum egyik, HelyszinEnum masik, AllapotEnum ... allapot) {
     this.nev = nev;
     this.leiras = leiras;
-    this.allapot = EnumSet.of(allapot);
     this.kulcs = kulcs;
     this.hely = EnumSet.of(egyik, masik);
+    this.allapot = Sets.newEnumSet(Arrays.asList(allapot), AllapotEnum.class);
   }
 
   @Override
@@ -64,13 +72,12 @@ enum AjtoEnum implements ElemInterface {
 
   @Override
   public void addAllapot(AllapotEnum allapot) {
-    this.allapot.clear(); // csak egyféle állapota lehet
     this.allapot.add(allapot);
   }
 
   @Override
   public void delAllapot(AllapotEnum allapot) {
-    throw new UnsupportedOperationException("Nincs szükség rá.");
+    this.allapot.remove(allapot);
   }
 
   @Override
