70a14c1dc0621562d39a9b64b88478d1c603b614
rolika@nostromo <weisz.roland@invitel.hu>
2017-06-04 19:30:50 +0200

    jelzőszótár kialakítása
    a terv az, hogy minden tárgynak lesz egy jelzője (ha más nem, NINCS)

---
+++ src/kaland/JelzoSzotar.java
@@ -0,0 +1,37 @@
+package kaland;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Azonos megnevezésű tárgyakat jelzőkkel lehet megkülönböztetni
+ *
+ * @author rolika
+ */
+enum JelzoSzotar implements Szotar {
+  KIS("kicsi", "nagy"),
+  NAGY("nagy");
+  
+  private final Set<String> szinonimak;
+
+  private JelzoSzotar(String... szinonimak) {
+    this.szinonimak = new HashSet<>(Arrays.asList(szinonimak));
+  }
+
+  @Override
+  public Szotar getSzoEnum(String szo) {
+    return szinonimak.contains(szo) ? this : null;
+  }
+  
+  /**
+   * A kalandelemek neve tartalmazza egyedül az esetleges jelzőt, erre ellenőriz rá
+   * 
+   * @param elem
+   * @return igaz, ha egyezik az elem jelzőjével
+   */
+  boolean checkJelzo(Elem elem) {
+    return szinonimak.stream().anyMatch(szo -> elem.getNev().contains(szo));
+  }
+
+}
