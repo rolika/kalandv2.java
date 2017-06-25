20a6d90bca8627b8d82a33b98a8dffd478fab2e4
rolika@nostromo <weisz.roland@invitel.hu>
2017-06-25 09:48:14 +0200

    ellenség-enum újraalkotva
    Állapotainak megfelelő üzenetek.

--- src/kaland/Ellenseg.java
+++ src/kaland/Ellenseg.java
@@ -5,34 +5,62 @@ import java.util.Arrays;
 import java.util.EnumSet;
 
 /**
- * A játék ellenségeit megvalósító enum.
- * Paraméterei: név - leírás - üzenet, ha megölte a játékost - üzenet, ha a játékos ölte meg -
- * ártalmatlan üzenet - pár, azaz hasznos tárgy (fegyver) ellene - állapot-enumset
- * 
+ * A játék ellenségeit megvalósító enum. Az ellenségnek három fokozata van: - passzív (EL): csak úgy
+ * van, ha a játékos nem piszkálja, nem csinál semmit - aktív (AKTIV): támadásra kész, a játékosnak
+ * vagy elmegy, vagy megtámadja, vagy: - támad (TAMAD): megtámadja a játékost. Jelenleg ez a játékos
+ * halálát jelenti. - halott (egyik se): a játékos megölte A játék ebből a négy üzenetből válogat a
+ * helyszín leírásakor, ha jelen van egy ellenség. Paraméterei: - név - leírás - passzív (nem csinál
+ * semmit) üzenet - aktív (mindjárt támad) üzenet - támad (megöli a játékost) üzenet - ártalmatlan
+ * üzenet (a játékos már megölte) - játékos megölte üzenet - helyszín, hatásos fegyver ellene (pár),
+ * állapotok
+ *
  * @author rolika
  */
 enum Ellenseg implements Elem {
 
-  ZOMBI("zombi", "Egy rothadó holttest, ami valamiképpen életre kelt. Fekete nyelve ide-oda vándorol a szájában, fogai céltalanul őrölnek egymáson. Iszonyú büdös!", "A zombi hirtelen kinyúl feléd és megragad! Rothadó fogai felé húz, érzed a szája irtóztató szagát, és ahogy a fogai beléd mélyednek. Aztán sötét lesz...", "Lesújtasz a zombira és szétloccsantod a fejét!", "Egy szétroncsolt fejű holttest van itt.", Helyszin.PINCE, Targy.BICSKA, Allapot.EL, Allapot.LATHATO);
-  
-  private final String nev, megol, meghal, halott, leiras;
+  ZOMBI(
+    "zombi",
+    "Egy rothadó holttest, ami valamiképpen életre kelt. Fekete nyelve ide-oda vándorol a szájában, fogai céltalanul őrölnek egymáson. Iszonyú büdös!",
+    "Egy zombi álldogál bizonytalan lábakon a közelben. Lassan dülöngél előre-hátra.",
+    "A zombi feszülten áll, opálos szemei rád szegeződnek!",
+    "A zombi hirtelen kinyúl feléd és megragad! Rothadó fogai felé húz, érzed a szája irtóztató szagát, és ahogy a fogai beléd mélyednek. Aztán sötét lesz...",
+    "Egy szétroncsolt fejű holttest van itt.",
+    "Lesújtasz a zombira és szétloccsantod a fejét!",
+    Helyszin.HAZ_ELOTT, Targy.BICSKA, Allapot.EL, Allapot.LATHATO);
+
+  private final String nev, leiras, passziv, aktiv, tamad, artalmatlan, megol;
   private final Helyszin hely;
   private final Elem par;
   private final JelzoSzotar jelzo;
   private final EnumSet<Allapot> allapot;
-  
- private Ellenseg(String nev, String leiras, String megol, String meghal, String halott,
-   Helyszin hely, Elem par, Allapot ... allapot) {
+
+  private Ellenseg(String nev, String leiras, String passziv, String aktiv, String tamad,
+    String artalmatlan, String megol,
+    Helyszin hely, Elem par, Allapot... allapot) {
     this.nev = nev;
     this.leiras = leiras;
+    this.passziv = passziv;
+    this.aktiv = aktiv;
+    this.tamad = tamad;
+    this.artalmatlan = artalmatlan;
     this.megol = megol;
-    this.meghal = meghal;
-    this.halott = halott;
     this.hely = hely;
     this.par = par;
     this.jelzo = JelzoSzotar.NINCS;
     this.allapot = Sets.newEnumSet(Arrays.asList(allapot), Allapot.class);
- }
+  }
+  
+  String uzenet() {
+    if (checkAllapot(Allapot.EL)) {
+      return passziv;
+    } else if (checkAllapot(Allapot.AKTIV)) {
+      return aktiv;
+    } else if (checkAllapot(Allapot.TAMAD)) {
+      return tamad;
+    } else {
+      return artalmatlan;
+    }
+  }
 
   @Override
   public String getNev() {
@@ -58,7 +86,7 @@ enum Ellenseg implements Elem {
   public void removeAllapot(Allapot... allapot) {
     this.allapot.removeAll(Arrays.asList(allapot));
   }
-  
+
   @Override
   public boolean checkAllapot(Allapot... allapot) {
     return this.allapot.containsAll(Arrays.asList(allapot));
@@ -84,16 +112,4 @@ enum Ellenseg implements Elem {
     return jelzo;
   }
 
-  String getMegol() {
-    return megol;
-  }
-
-  String getMeghal() {
-    return meghal;
-  }
-
-  String getHalott() {
-    return halott;
-  }
-  
 }
